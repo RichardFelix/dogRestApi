474ec831635ccb11c5ab08f4dc8bd223f4259331
Richard Felix <me@richardcfelix.com>
2015-12-16 15:42:37 -0500

    commented out custom views folder location

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express'),
     port = process.env.PORT || 3000,
     dogApi = require('./dogApi');
 
+//app.set('views', __dirname + '/public/views');
 app.set('view engine', 'jade');
 
 app.use(dogApi.bodyParser.urlencoded({ extended: false }));
@@ -17,12 +18,12 @@ app.get('/api/new', function(req, res){
     res.render('create');
 });
 
-app.post('/api', dogApi.newDog );
+app.post('/api', dogApi.newDog ); 
 app.put('/api/:id', dogApi.put );
-app.delete('/api/:id', dogApi.deleteDog );
 app.get('/api', dogApi.findAll );
 app.get('/api/:id', dogApi.findOne );
 app.get('/api/:id/edit', dogApi.edit );
+app.delete('/api/:id', dogApi.deleteDog );
 
 app.get('*', function(req, res){
     res.redirect('/');
